248a615b8535d077e5d828d97e2c774657e6db2e
Udoh <udohabasionosunday@gmail.com>
2023-03-28 14:42:41 +0100

    Final changes added

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,30 +1,48 @@
 import { useDispatch } from "react-redux";
 import { completeTodoThunk, removeTodoThunk } from "../app/thunks";
 import "./TodoListItem.css";
+import styled from "styled-components";
+
+const BorderButtom = styled.div`
+  border-radius: 8px;
+  border-bottom: ${(props) =>
+    new Date(props.todo.createdAt) < new Date(Date.now() - 86400000)
+      ? "3px solid red"
+      : "none"};
+`;
+
+const NoBorderButtom = styled.div``;
 
 const TodoListItem = ({ todo }) => {
   const dispatch = useDispatch();
 
+  const Container = todo.isCompleted ? NoBorderButtom : BorderButtom;
+
   return (
-    <div className="todo-item-container">
-      <h3>{todo.text}</h3>
-      <div className="buttons-container">
-        {!todo.isCompleted && (
+    <Container todo={todo}>
+      <div className="todo-item-container">
+        <h3>{todo.text}</h3>
+        <time dateTime={todo.createdAt}>
+          Created at&nbsp;{new Date(todo.createdAt).toLocaleDateString()}
+        </time>
+        <div className="buttons-container">
+          {!todo.isCompleted && (
+            <button
+              className="completed-button"
+              onClick={() => dispatch(completeTodoThunk(todo.id))}
+            >
+              Mark As Completed
+            </button>
+          )}
           <button
-            className="completed-button"
-            onClick={() => dispatch(completeTodoThunk(todo.id))}
+            className="remove-button"
+            onClick={() => dispatch(removeTodoThunk(todo.id))}
           >
-            Mark As Completed
+            Remove
           </button>
-        )}
-        <button
-          className="remove-button"
-          onClick={() => dispatch(removeTodoThunk(todo.id))}
-        >
-          Remove
-        </button>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
